7ddda95fe92515dc1ad99046884f8ad8f846fa97
Chris Perkins <chris@percious.com>
2013-02-11 20:18:48 -0700

    fixes to allow static files to work.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tw2.ckeditor',
-    version='2.0',
+    version='2.0.1',
     description='',
     author='Christopher Perkins (percious)',
     author_email='',
